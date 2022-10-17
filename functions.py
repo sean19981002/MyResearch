0e2d4b9bc1b991b671fec84fd4bded0909228797
sean19981002 <115693168+sean19981002@users.noreply.github.com>
2022-10-17 20:45:50 +0800

    10/17 須大改 Get_User_Tweets()
    <ReferencedTweet id=1579562044449513472 type=retweeted
    這是下載下來的資料格式，type 為 list，須先搞懂

--- functions.py
+++ functions.py
@@ -211,18 +211,19 @@ def Get_User_Tweets(token:str, target_users:list, biden_tweets:list, num:int, re
             id=id, 
             end_time='2022-10-11T00:00:00Z',
             start_time='2022-10-03T00:00:00Z',
-            tweet_fields=['context_annotations','created_at','referenced_tweets'],
+            tweet_fields=['created_at','referenced_tweets'],
             expansions=['referenced_tweets.id'], 
             max_results=100).flatten(limit=100000000):
 
-            ref_tweet = str(tweet.referenced_tweets)
-            ref_tweet = ref_tweet.replace('[<ReferencedTweet id=','').replace(' type=','').replace('replied_to]','').replace('retweeted]','').replace('quoted]','')
+            ref_tweet = tweet.referenced_tweets
+            #ref_tweet = ref_tweet.replace('[<ReferencedTweet id=','').replace(' type=','').replace('replied_to]','').replace('retweeted]','').replace('quoted]','')
             with open("%d.txt" % num, "w") as f:
-                f.write(ref_tweet)
-            if ref_tweet != None: # if this tweet of the user has retweet any tweet
-                ref_tweet = int(ref_tweet)
-                if ref_tweet in biden_tweets:
-                    tmp = [tweet.id, tweet.created_at, ref_tweet]
+                for i in ref_tweet:
+                    f.write(str(i) + "\n")
+            if ref_tweet.id != None: # if this tweet of the user has retweet any tweet
+                #ref_tweet = int(ref_tweet)
+                if int(ref_tweet.id) in biden_tweets:
+                    tmp = [tweet.id, tweet.created_at, ref_tweet.id]
                     colloection.append(tmp)
         
         if len(colloection) > 0:
